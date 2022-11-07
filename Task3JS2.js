655eb4925103d1044b5f7bf14259cacc0828c42d
Wizra Raudha Filwathan <100467903+filwathan@users.noreply.github.com>
2022-11-07 10:51:28 +0700

    Add files via upload

---
+++ Task3JS2.js
@@ -0,0 +1,39 @@
+//#region nomer 1
+
+//#endregion
+
+//#region nomer 2
+const searchName = function(lertters, manyitems, cb) {
+    const name = [
+    'Abagail','Alexandra','Alison', 'Amanda', 'Angela', 'Bella',
+    'Carol', 'Caroline', 'Carolyn', 'Deirde', 'Diana', 'Elizabeth',
+    'Ella', 'Faith', 'Olivia', 'Penelope'
+    ];
+
+    let result = name.forEach(element => {
+        console.log(element);
+    });
+    
+    // name[0].includes('an') ? name[0] : null ;
+
+
+    // callback(result);
+
+}
+
+const callback = function (output) {
+    console.log(output);
+}
+//#endregion
+
+//#region nomer 3
+
+//#endregion
+
+//#region nomer 4
+
+//#endregion
+
+//#region nomer 5
+
+//#endregion
